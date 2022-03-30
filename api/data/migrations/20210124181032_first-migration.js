260dcaa1a0b36b1dd5f44bca4e96ce49fe208b59
andrewhunt21 <andrewhunt21@icloud.com>
2022-03-29 20:33:53 -0700

    created data migration table

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -1,13 +1,13 @@
 exports.up = async (knex) => {
   await knex.schema
-    .createTable('users', (users) => {
-      users.increments('user_id')
-      users.string('username', 200).notNullable()
-      users.string('password', 200).notNullable()
-      users.timestamps(false, true)
+    .createTable('voters', (voters) => {
+      voters.increments('voter_id')
+      voters.string('name', 100).notNullable()
+      voters.string('gender', 200).notNullable()
+      voters.date('birthday').notNullable()
     })
 }
 
 exports.down = async (knex) => {
-  await knex.schema.dropTableIfExists('users')
+  await knex.schema.dropTableIfExists('voters')
 }
